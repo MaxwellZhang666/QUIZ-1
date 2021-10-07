9bc4ac6bfaaee8deabf4209f917ff622422ddc5a
Maxwell Zhang <Yiran.zhang@ontariotechu.net>
2021-10-07 17:06:10 -0400

    midterm

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 5.5f;
+    [SerializeField] float jumpForce = 2f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+        if (Input.GetButtonDown("Jump")&& IsGround())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+        bool IsGround()
+        {
+            return Physics.CheckSphere(groundCheck.position, .1f, ground);
+        }
+    }
+}
